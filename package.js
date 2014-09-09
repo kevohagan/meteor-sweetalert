890d939049b2afa47333d92dc4a6471c554cf003
Kevin O'Hagan <kevinohagan@gmail.com>
2014-09-09 11:14:03 +0200

    project initialized

---
+++ package.js
@@ -0,0 +1,21 @@
+Package.describe({
+    name: 'kevohagan:ramda',
+    summary: 'A practical functional library for Javascript programmers.',
+    version: '0.1.0',
+    git: 'https://github.com/kevohagan/meteor-ramda.git',
+    upstreamVersion: "0.1.0"
+});
+
+Package.onUse(function(api) {
+
+    api.versionsFrom("METEOR@0.9.1.1");
+
+    api.use('jquery', 'client');
+
+    api.add_files([
+        'lib/ramda.js',
+    ], 'client');
+
+    if(api.export)
+        api.export('ramda');
+})
